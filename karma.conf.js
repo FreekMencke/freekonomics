f20cf905ec4f5c77f6e0f9995bfd5c7be24de740
Freek Mencke <freek_mencke@outlook.com>
2023-03-12 18:51:55 +0100

    refactor(angular.json): updated psd2-overview to freekomics, also emit robots.txt on predeploy

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/psd2-overview'),
+      dir: require('path').join(__dirname, './coverage/freekonomics'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
